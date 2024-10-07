66a5b8dace88717b309001cae80d792b38a8994c
Grigoli2001 <gpatsatsiausa@gmail.com>
2024-10-07 20:28:36 +0200

    added authcontext

---
+++ src/types/auth.types.ts
@@ -0,0 +1,36 @@
+export interface AuthContextType {
+  user: MeResponse | null;
+
+  isAuthenticated: boolean;
+  isSuperAdmin: boolean;
+
+  login: (
+    email: string,
+    password: string,
+    rememberMe: boolean
+  ) => Promise<void>;
+
+  logout: () => Promise<void>;
+}
+
+export interface MeResponse {
+  _id: string;
+  username: string;
+  email: string;
+  superAdmin: boolean;
+  created_at: string;
+  updated_at: string;
+  __v: number;
+}
+
+export interface LoginResponse {
+  accessToken: string;
+}
+
+export interface LogoutResponse {
+  message: string;
+}
+
+export interface RefreshTokenResponse {
+  accessToken: string;
+}
